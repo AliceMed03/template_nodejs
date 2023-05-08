6a5984aba4322d65338c82e1f1ca1298b06eb7bf
AliceMed03 <alicemedeirosds@gmail.com>
2023-05-08 19:30:42 -0300

    Atualização

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+//aplicação pelo metodo get vai buscar a rota localhost:3000/
 app.get('/', (req, res) => {
-  res.send('Hello Alice!')
+  res.send('Olá Alice!!!')
+})
+
+//exemplo relacionado a filmes localhost:3000/filmes
+app.use(express.json()); // começa a receber json
+app.post('/livros', (req,res)=> {
+
+//const titulo = req.body.titulo;
+//const genero = req.body.genero;
+const {autor,nome,editora} = req.body;
+res.send( `Autor: ${autor} - Nome: ${nome} - Editora: ${editora}, recebido` );
 })
 
 app.listen(port, () => {
